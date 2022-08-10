0cf597bc9c16c3cb2b5f1b2d0ca9272e9226219b
Elfin Git <musongoletracy@gmail.com>
2022-08-10 13:30:48 +0300

    delete server.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -12,6 +13,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
+    new CleanWebpackPlugin()
 
   ],
   output: {
