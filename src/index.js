6ebff2e5cbdba75c496e64541213e0bb0b35e860
Elfin Git <musongoletracy@gmail.com>
2022-08-10 23:58:13 +0300

    Add active class for button

--- src/index.js
+++ src/index.js
@@ -5,4 +5,13 @@ import refreshIcon from './assets/Refresh_icon.png';
 const myIcon = new Image();
 myIcon.src = refreshIcon;
 
+const inputBox = document.querySelector('#add-new');
+const addBtn = document.querySelector('#plus');
+inputBox.onkeyup = () => {
+  const newData = inputBox.value;
+  if (newData.trim() !== 0) {
+    addBtn.classList.add('active');
+  }
+};
+
 window.onload = UI.displayTasks();
