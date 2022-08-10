b074a06f900eacc6d2686921b414e265271ab817
Elfin Git <musongoletracy@gmail.com>
2022-08-10 22:47:43 +0300

    Style list items

--- src/assets/create.js
+++ src/assets/create.js
@@ -17,20 +17,18 @@ class UI {
     const container = document.querySelector('#list-items');
     container.innerHTML = '';
     toDo.forEach((list) => {
-      const label = document.createElement('label');
       const listCont = document.createElement('div');
       listCont.className = 'single-task';
+      const label = document.createElement('label');
       label.setAttribute('for', 'id1');
+      label.innerHTML = list.description;
       const toDo = document.createElement('input');
       toDo.setAttribute('type', 'checkbox');
       toDo.id = 'id1';
-      const para = document.createElement('p');
-      para.className = 'task';
-      para.innerHTML = list.description;
       const dots = document.createElement('i');
       dots.className = 'bi bi-three-dots-vertical';
+      listCont.append(toDo, label, dots);
       container.appendChild(listCont);
-      listCont.append(label, toDo, para, dots);
     });
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import './style.css';
 import UI from './assets/create.js';
+import refreshIcon from './assets/Refresh_icon.png';
+
+const myIcon = new Image();
+myIcon.src = refreshIcon;
 
 window.onload = UI.displayTasks();
