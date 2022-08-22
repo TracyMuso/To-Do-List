804e0fab640077d702fedfe108f5489c2b690596
Elfin Git <musongoletracy@gmail.com>
2022-08-22 17:12:46 +0300

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
 
   ],
   output: {
-    filename: '[name].js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
     publicPath: '/',
