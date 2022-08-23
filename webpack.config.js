8aa8ccef02de8c26f6bd80201ba07638d9ef415b
Elfin Git <musongoletracy@gmail.com>
2022-08-23 11:54:30 +0300

    update dist dir

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,48 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.js'),
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new CleanWebpackPlugin(),
-  ],
   output: {
-    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
+    filename: '[name][contenthash].js',
     clean: true,
-    publicPath: '/',
+    assetModuleFilename: '[name][ext]',
   },
-
-  optimization: {
-    runtimeChunk: 'single',
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
   },
-
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|gif)$/i,
         type: 'asset/resource',
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
-      {
-        test: /\.html$/i,
-        use: ['html-loader'],
-      },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'To-Do-List App',
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+  ],
 };
