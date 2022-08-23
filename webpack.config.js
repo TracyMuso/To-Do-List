80e6611cea2bb2713b1894e796ef0c871a104107
Elfin Git <musongoletracy@gmail.com>
2022-08-23 10:18:49 +0300

    rename entry point

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
 
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
     publicPath: '/',
